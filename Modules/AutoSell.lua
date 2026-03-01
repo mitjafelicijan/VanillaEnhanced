73c47f3847f8ac453e6c0b1733143cf6c4572161
Mitja Felicijan <mitja.felicijan@gmail.com>
2026-03-01 15:04:54 +0100

    Auto sell

--- Modules/AutoSell.lua
+++ Modules/AutoSell.lua
@@ -18,20 +18,48 @@ end
 
 module.plug = CreateFrame("Frame", module.identifier)
 module.plug:RegisterEvent("MERCHANT_SHOW")
+module.plug:RegisterEvent("MERCHANT_CLOSED")
+module.plug:Hide()
+
+local timeSinceLastUpdate = 0
+local updateInterval = 0.2
 
 module.plug:SetScript("OnEvent", function()
-	if not VE.isModuleEnabled(module.identifier) then return end
+	if event == "MERCHANT_SHOW" then
+		if not VE.isModuleEnabled(module.identifier) then return end
 
-	-- Always open all bags.
-	OpenAllBags()
+		-- Always open all bags.
+		OpenAllBags()
+
+		module.plug:Show()
+	elseif event == "MERCHANT_CLOSED" then
+		module.plug:Hide()
+	end
+end)
+
+module.plug:SetScript("OnUpdate", function()
+	timeSinceLastUpdate = timeSinceLastUpdate + arg1
+	if timeSinceLastUpdate < updateInterval then return end
+	timeSinceLastUpdate = 0
 
 	if IsShiftKeyDown() then return end
-	for bag = 0,4,1 do
-		for slot = 1, GetContainerNumSlots(bag),1 do
-			local name = GetContainerItemLink(bag, slot)
-			if name and string.find(name, "ff9d9d9d") then
-				DEFAULT_CHAT_FRAME:AddMessage("Selling "..name)
-				UseContainerItem(bag, slot)
+
+	for bag = 0, 4 do
+		for slot = 1, GetContainerNumSlots(bag) do
+			local texture, count, locked = GetContainerItemInfo(bag, slot)
+			if texture and not locked then
+				local link = GetContainerItemLink(bag, slot)
+				if link then
+					local _, _, id = string.find(link, "item:(%d+)")
+					if id then
+						local _, _, quality = GetItemInfo(id)
+						if quality == 0 then
+							DEFAULT_CHAT_FRAME:AddMessage("Selling " .. link)
+							UseContainerItem(bag, slot)
+							return
+						end
+					end
+				end
 			end
 		end
 	end
