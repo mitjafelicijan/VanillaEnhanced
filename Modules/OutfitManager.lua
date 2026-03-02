c1895881d5d45bba517ab167185bca68fad6ca13
Mitja Felicijan <mitja.felicijan@gmail.com>
2026-03-02 06:04:56 +0100

    Outfit manager

---
+++ Modules/OutfitManager.lua
@@ -0,0 +1,27 @@
+local module = VE.registerModule({
+	identifier = "OutfitManager",
+	meta = {
+		label = "Outfit Manager",
+		description = "...",
+	},
+	plug = nil,
+	superWoWRequired = false,
+	config = {},
+	data = {},
+})
+
+-- Check for SuperWoW dependency.
+if not VE.superWoWCheck(module) then
+	VE.iprint(string.format("No SuperWoW detected. %s is NOT enabled.", module.meta.label))
+	return
+end
+
+module.plug = CreateFrame("Frame", module.identifier)
+module.plug:RegisterEvent("PLAYER_ENTERING_WORLD")
+
+module.plug:SetScript("OnEvent", function()
+	if not VE.isModuleEnabled(module.identifier) then return end
+
+	-- Code goes here
+	VE.print("....outfit manager")
+end)
--- Panels/Controls.lua
+++ Panels/Controls.lua
@@ -132,6 +132,15 @@ VE.panels.Controls = function(parent)
 			end, module.superWoWRequired)
 		end
 	end
+	
+	do
+		local module = VE.getModule("OutfitManager")
+		if module then
+			VE.elements.Checkbox(frame, 270, -330, 220, module.meta.label, module.meta.description, nil, module.enabled, function(checked)
+				if checked then VE.enableModule(module.identifier) else VE.disableModule(module.identifier) end
+			end, module.superWoWRequired)
+		end
+	end
 
 	if VE.config.Debug then VE.dframe(frame, 0.0, 1.0, 1.0, 0.2) end
 
