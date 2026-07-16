72229e148b05fcc43ff539696177993a9f9ec135
Mitja Felicijan <mitja.felicijan@gmail.com>
2026-07-17 00:41:16 +0200

    Reintroduce Previous Raid Frames

--- Panels/RaidFrames.lua
+++ Panels/RaidFrames.lua
@@ -24,10 +24,13 @@ VE.panels.RaidFrames = function(parent)
 		VE.SetUVar("SHOW_CASTABLE_BUFFS", checked)
 	end)
 
+	if not VanillaEnhancedOptions["CompactFramesUnitWidth"] then VanillaEnhancedOptions["CompactFramesUnitWidth"] = 76 end
+	if not VanillaEnhancedOptions["CompactFramesUnitHeight"] then VanillaEnhancedOptions["CompactFramesUnitHeight"] = 38 end
+
 	-- Right column
 
 	do
-		local module = VE.getModule("CompactRaidFrames")
+		local module = VE.getModule("RaidTargetMarkers")
 		if module then
 			VE.elements.Checkbox(frame, 270, -20, 220, module.meta.label, module.meta.description, nil, module.enabled, function(checked)
 				if checked then VE.enableModule(module.identifier) else VE.disableModule(module.identifier) end
@@ -36,11 +39,44 @@ VE.panels.RaidFrames = function(parent)
 	end
 
 	do
-		local module = VE.getModule("RaidTargetMarkers")
+		local module = VE.getModule("CompactFrames")
 		if module then
-			VE.elements.Checkbox(frame, 270, -50, 220, module.meta.label, module.meta.description, nil, module.enabled, function(checked)
+			VE.elements.Checkbox(frame, 270, -60, 220, module.meta.label, module.meta.description, nil, module.enabled, function(checked)
 				if checked then VE.enableModule(module.identifier) else VE.disableModule(module.identifier) end
 			end, module.superWoWRequired)
+
+			local showPets = VE.getOption("CompactFramesShowPets")
+			if showPets then
+				VE.elements.Checkbox(frame, 290, -90, 200, showPets.meta.label, showPets.meta.description, nil, showPets.enabled, function(checked)
+					if checked then VE.enableOption(showPets.identifier) else VE.disableOption(showPets.identifier) end
+				end, showPets.superWoWRequired)
+			end
+
+			local showFocus = VE.getOption("CompactFramesShowFocusFrames")
+			if showFocus then
+				VE.elements.Checkbox(frame, 290, -120, 200, showFocus.meta.label, showFocus.meta.description, nil, showFocus.enabled, function(checked)
+					if checked then VE.enableOption(showFocus.identifier) else VE.disableOption(showFocus.identifier) end
+				end, showFocus.superWoWRequired)
+			end
+
+			VE.elements.DropDown(frame, 300, -150, 160, "Display Auras", VanillaEnhancedOptions["CompactFramesAuras"], {
+				{ key = 0, text = "None", tooltip = nil },
+				{ key = 1, text = "Buffs", tooltip = nil },
+				{ key = 2, text = "Debuffs", tooltip = nil },
+				{ key = 3, text = "HOTs", tooltip = nil },
+			}, function(key)
+				VanillaEnhancedOptions["CompactFramesAuras"] = key
+			end)
+
+			local widthSlider
+			widthSlider = VE.elements.Slider(frame, 270, -220, 200, "Unit Frame Width [76]", nil, nil, 60, 150, 1, VanillaEnhancedOptions["CompactFramesUnitWidth"], function(val)
+				VanillaEnhancedOptions["CompactFramesUnitWidth"] = val
+			end)
+
+			local heightSlider
+			heightSlider = VE.elements.Slider(frame, 270, -270, 200, "Unit Frame Height [38]", nil, nil, 25, 80, 1, VanillaEnhancedOptions["CompactFramesUnitHeight"], function(val)
+				VanillaEnhancedOptions["CompactFramesUnitHeight"] = val
+			end)
 		end
 	end
 
