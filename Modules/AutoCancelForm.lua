3827def4e3efdf697e9d2a2ce39e677e8a3084b9
Mitja Felicijan <mitja.felicijan@gmail.com>
2026-07-18 19:37:35 +0200

    Improve auto cancel forms

--- Modules/AutoCancelForm.lua
+++ Modules/AutoCancelForm.lua
@@ -6,7 +6,16 @@ local module = VE.registerModule({
 	},
 	plug = nil,
 	superWoWRequired = false,
-	config = {},
+	-- https://vanilla-wow-archive.fandom.com/wiki/WoW_Constants/Errors
+	config = {
+		cancelFormErrors = {
+			[SPELL_FAILED_NOT_SHAPESHIFT] = true,
+			[ERR_CANT_INTERACT_SHAPESHIFTED] = true,
+			[ERR_NOT_WHILE_SHAPESHIFTED] = true,
+			[ERR_MOUNT_SHAPESHIFTED] = true,
+			[ERR_NO_ITEMS_WHILE_SHAPESHIFTED] = true,
+		}
+	},
 	data = {},
 })
 
@@ -23,10 +32,7 @@ module.plug:SetScript("OnEvent", function()
 	if not VE.isModuleEnabled(module.identifier) then return end
 	if not UnitClass("player") == "Druid" or not UnitClass("player") == "Shaman" then return end
 
-	-- https://github.com/refaim/Turtle-WoW-UI-Source/blob/master/Interface/FrameXML/GlobalStrings.lua
-	if arg1 == SPELL_FAILED_NOT_SHAPESHIFT or
-		arg1 == ERR_CANT_INTERACT_SHAPESHIFTED or
-		arg1 == ERR_NOT_WHILE_SHAPESHIFTED then
+	if module.config.cancelFormErrors[arg1] then
 		SlashCmdList["CANCELFORM"]()
 	end
 end)
